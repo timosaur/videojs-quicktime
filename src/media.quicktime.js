2e42db1ad515315e8222e20759f62bf68d7d8aaa
Timothy Wang <timothy.wang@zefr.com>
2013-07-15 19:28:59 -0700

    Messy test that works with "video" object outside of video tag

--- src/media.quicktime.js
+++ src/media.quicktime.js
@@ -9,172 +9,217 @@
  * @param {Function=} ready
  * @constructor
  */
-videojs.Youtube = videojs.MediaTechController.extend({
+
+
+/* define function that adds another function as a DOM event listener */
+    function myAddListener(obj, evt, handler, captures)
+        {
+        if ( document.addEventListener )
+            obj.addEventListener(evt, handler, captures);
+        else
+            // IE
+            obj.attachEvent('on' + evt, handler);
+       }
+ 
+   /* define functions that register each listener */
+    function RegisterListener(eventName, objID, embedID, listenerFcn)
+    {
+        var obj = document.getElementById(objID);
+        if ( !obj )
+            obj = document.getElementById(embedID);
+        if ( obj )
+            myAddListener(obj, eventName, listenerFcn, false);
+    }
+ 
+    /* define a single function that registers all listeners to call onload */
+    // function RegisterListeners()
+    // {
+    //    RegisterListener('qt_progress', 'movie1', 'qtmovie_embed', showProgress);
+    //    RegisterListener('qt_load', 'movie1', 'qtmovie_embed', movieLoaded);
+    // }
+
+videojs.Quicktime = videojs.MediaTechController.extend({
   init: function(player, options, ready){
+    console.log("init", player, options, ready);
     videojs.MediaTechController.call(this, player, options, ready);
     
-    this.features.fullscreenResize = true;
+    console.log(this);
+    // this.features.fullscreenResize = true;
     
     this.player_ = player;
     this.player_el_ = document.getElementById(this.player_.id());
 
+    console.log(this.player_el_);
+
+    console.log("options", this.player_.options());
+
+    console.log("controls", this.player_.controls());
+    console.log("controls", this.player_.controlBar);
+    // window.controlbar = this.player_.controlBar.el();
+    window.player = this.player_;
     // Disable lockShowing because YouTube controls are there
-    if (this.player_.options().ytcontrols){
-      this.player_.controls(false);
-    }
+    // if (this.player_.options().ytcontrols){
+    //   this.player_.controls(false);
+    // }
     
     // Regex that parse the video ID for any YouTube URL
-    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-    var match = player.options().src.match(regExp);
+    // var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    // var match = player.options().src.match(regExp);
 
-    if (match && match[2].length == 11){
-      this.videoId = match[2];
+    // if (match && match[2].length == 11){
+    //   this.videoId = match[2];
 
-      // Show the YouTube poster only if we don't use YouTube poster (otherwise the controls pop, it's not nice)
-      if (!this.player_.options().ytcontrols){
-        // Set the YouTube poster only if none is specified
-        if (typeof this.player_.poster() == 'undefined') {
-          this.player_.poster('http://img.youtube.com/vi/' + this.videoId + '/0.jpg');
-        }
+    //   // Show the YouTube poster only if we don't use YouTube poster (otherwise the controls pop, it's not nice)
+    //   if (!this.player_.options().ytcontrols){
+    //     // Set the YouTube poster only if none is specified
+    //     if (typeof this.player_.poster() == 'undefined') {
+    //       this.player_.poster('http://img.youtube.com/vi/' + this.videoId + '/0.jpg');
+    //     }
 
-        // Cover the entire iframe to have the same poster than YouTube
-        // Doesn't exist right away because the DOM hasn't created it
-        var self = this;
-        setTimeout(function(){ self.player_.posterImage.el().style.backgroundSize = 'cover'; }, 50);
-      }
-    } else {
-      this.videoId = '';
-    }
+    //     // Cover the entire iframe to have the same poster than YouTube
+    //     // Doesn't exist right away because the DOM hasn't created it
+    //     var self = this;
+    //     setTimeout(function(){ self.player_.posterImage.el().style.backgroundSize = 'cover'; }, 50);
+    //   }
+    // } else {
+    //   this.videoId = '';
+    // }
 
-    this.id_ = this.player_.id() + '_youtube_api';
+    // this.id_ = this.player_.id() + '_youtube_api';
 
-    this.el_ = videojs.Component.prototype.createEl('iframe', {
-      id: this.id_,
-      className: 'vjs-tech',
-      scrolling: 'no',
-      marginWidth: 0,
-      marginHeight: 0,
-      frameBorder: 0,
-      webkitAllowFullScreen: '',
-      mozallowfullscreen: '',
-      allowFullScreen: ''
-    });
+    // this.el_ = videojs.Component.prototype.createEl('object', {
+    //   id: this.id_,
+    //   className: 'vjs-tech',
+    //   scrolling: 'no',
+    //   marginWidth: 0,
+    //   marginHeight: 0,
+    //   frameBorder: 0,
+    //   webkitAllowFullScreen: '',
+    //   mozallowfullscreen: '',
+    //   allowFullScreen: ''
+    // });
     
-    this.player_el_.insertBefore(this.el_, this.player_el_.firstChild);
+    // this.player_el_.insertBefore(this.el_, this.player_el_.firstChild);
     
-    var params = {
-      enablejsapi: 1,
-      iv_load_policy: 3,
-      playerapiid: this.id(),
-      disablekb: 1,
-      wmode: 'transparent',
-      controls: (this.player_.options().ytcontrols)?1:0,
-      showinfo: 0,
-      modestbranding: 1,
-      rel: 0,
-      autoplay: (this.player_.options().autoplay)?1:0,
-      loop: (this.player_.options().loop)?1:0
-    };
+    // var params = {
+    //   enablejsapi: 1,
+    //   iv_load_policy: 3,
+    //   playerapiid: this.id(),
+    //   disablekb: 1,
+    //   wmode: 'transparent',
+    //   controls: (this.player_.options().ytcontrols)?1:0,
+    //   showinfo: 0,
+    //   modestbranding: 1,
+    //   rel: 0,
+    //   autoplay: (this.player_.options().autoplay)?1:0,
+    //   loop: (this.player_.options().loop)?1:0
+    // };
     
-    // Make autoplay work for iOS
-    if (this.player_.options().autoplay) {
-      this.playOnReady = true;
-    }
+    // // Make autoplay work for iOS
+    // if (this.player_.options().autoplay) {
+    //   this.playOnReady = true;
+    // }
     
-    // Check if we have a playlist
-    var regExp = /[?&]list=([^#\&\?]+)/;
-    var match = player.options().src.match(regExp);
+    // // Check if we have a playlist
+    // var regExp = /[?&]list=([^#\&\?]+)/;
+    // var match = player.options().src.match(regExp);
     
-    if (match != null && match.length > 1) {
-      params.list = match[1];
-    }
+    // if (match != null && match.length > 1) {
+    //   params.list = match[1];
+    // }
     
-    // If we are not on a server, don't specify the origin (it will crash)
-    if (window.location.protocol != 'file:') {
-      params.origin = window.location.protocol + '//' + window.location.hostname;
-    }
+    // // If we are not on a server, don't specify the origin (it will crash)
+    // if (window.location.protocol != 'file:') {
+    //   params.origin = window.location.protocol + '//' + window.location.hostname;
+    // }
 
-    this.el_.src = 'http://www.youtube.com/embed/' + this.videoId + '?' + videojs.Youtube.makeQueryString(params);
+    // this.el_.src = 'http://www.youtube.com/embed/' + this.videoId + '?' + videojs.Quicktime.makeQueryString(params);
 
-    if (this.player_.options().ytcontrols){
-      // Remove the big play button and the control bar, we use Vimeo controls
-      // Doesn't exist right away because the DOM hasn't created it
-      var self = this;
-      setTimeout(function(){ 
-        var bigPlayDom = self.player_.bigPlayButton.el();
-        bigPlayDom.parentNode.removeChild(bigPlayDom);
+    // if (this.player_.options().ytcontrols){
+    //   // Remove the big play button and the control bar, we use Vimeo controls
+    //   // Doesn't exist right away because the DOM hasn't created it
+    //   var self = this;
+    //   setTimeout(function(){ 
+    //     var bigPlayDom = self.player_.bigPlayButton.el();
+    //     bigPlayDom.parentNode.removeChild(bigPlayDom);
         
-        var controlBarDom = self.player_.controlBar.el();
-        controlBarDom.parentNode.removeChild(controlBarDom);
-      }, 50);
-    }
+    //     var controlBarDom = self.player_.controlBar.el();
+    //     controlBarDom.parentNode.removeChild(controlBarDom);
+    //   }, 50);
+    // }
 
-    if (videojs.Youtube.apiReady){
-      this.loadYoutube();
-    } else {
-      // Add to the queue because the YouTube API is not ready
-      videojs.Youtube.loadingQueue.push(this);
+    // this.loadQuicktime();
 
-      // Load the YouTube API if it is the first YouTube video
-      if(!videojs.Youtube.apiLoading){
-        var tag = document.createElement('script');
-        tag.src = 'http://www.youtube.com/iframe_api';
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-        videojs.Youtube.apiLoading = true;
-      }
-    }
+    // Load quicktime object
+    this.qtplayer = document.video;
+    // console.log("qtplayer", this.qtplayer);
+
+    // this.onReady();
+    // console.log("loaded");
+    var me = this;
+
+    RegisterListener('qt_load', 'video', 'video', function() {
+      me.onReady();
+    });
   }
 });
 
-videojs.Youtube.prototype.dispose = function(){
-  this.ytplayer.destroy();
-  videojs.MediaTechController.prototype.dispose.call(this);
-};
+// videojs.Quicktime.prototype.dispose = function(){
+//   this.ytplayer.destroy();
+//   videojs.MediaTechController.prototype.dispose.call(this);
+// };
 
-videojs.Youtube.prototype.play = function(){
-  if (this.isReady_){ 
-    this.ytplayer.playVideo(); 
+videojs.Quicktime.prototype.play = function(){
+  console.log("play");
+  if (this.isReady_){
+    this.qtplayer.Play();
   } else { 
     // We will play it when the API will be ready
     this.playOnReady = true;
   }
 };
 
-videojs.Youtube.prototype.pause = function(){ this.ytplayer.pauseVideo(); };
-videojs.Youtube.prototype.paused = function(){
+videojs.Quicktime.prototype.pause = function(){ this.qtplayer.Stop(); };
+videojs.Quicktime.prototype.paused = function(){
   return this.lastState !== YT.PlayerState.PLAYING &&
          this.lastState !== YT.PlayerState.BUFFERING;
 };
 
-videojs.Youtube.prototype.currentTime = function(){ return this.ytplayer.getCurrentTime(); };
+videojs.Quicktime.prototype.currentTime = function(){
+  console.log("Current Time", this.qtplayer.GetTime());
+  return this.qtplayer.GetTime();
+};
 
-videojs.Youtube.prototype.setCurrentTime = function(seconds){
-  this.ytplayer.seekTo(seconds, true);
+videojs.Quicktime.prototype.setCurrentTime = function(seconds){
+  this.qtplayer.SetTime(seconds);
   this.player_.trigger('timeupdate');
 };
 
-videojs.Youtube.prototype.duration = function(){ return this.ytplayer.getDuration(); };
-videojs.Youtube.prototype.buffered = function(){
-  var loadedBytes = this.ytplayer.getVideoBytesLoaded();
-  var totalBytes = this.ytplayer.getVideoBytesTotal();
+videojs.Quicktime.prototype.duration = function(){
+  console.log("Duration", this.qtplayer.GetDuration());
+  return this.qtplayer.GetDuration();
+};
+videojs.Quicktime.prototype.buffered = function(){
+  var loadedBytes = this.qtplayer.GetMaxBytesLoaded();
+  var totalBytes = this.qtplayer.GetMovieSize();
   if (!loadedBytes || !totalBytes) return 0;
 
-  var duration = this.ytplayer.getDuration();
+  var duration = this.qtplayer.GetDuration();
   var secondsBuffered = (loadedBytes / totalBytes) * duration;
-  var secondsOffset = (this.ytplayer.getVideoStartBytes() / totalBytes) * duration;
+  // var secondsOffset = (this.qtplayer.getVideoStartBytes() / totalBytes) * duration;
+  var secondsOffset = 0;
   return videojs.createTimeRange(secondsOffset, secondsOffset + secondsBuffered);
 };
 
-videojs.Youtube.prototype.volume = function() { 
+videojs.Quicktime.prototype.volume = function() { 
   if (isNaN(this.volumeVal)) {
-    this.volumeVal = this.ytplayer.getVolume() / 100.0;
+    this.volumeVal = this.qtplayer.GetVolume();
   }
-
+  console.log("Volume", this.volumeVal);
   return this.volumeVal;
 };
 
-videojs.Youtube.prototype.setVolume = function(percentAsDecimal){
+videojs.Quicktime.prototype.setVolume = function(percentAsDecimal){
   if (percentAsDecimal && percentAsDecimal != this.volumeVal) {
     this.ytplayer.setVolume(percentAsDecimal * 100.0); 
     this.volumeVal = percentAsDecimal;
@@ -182,8 +227,11 @@ videojs.Youtube.prototype.setVolume = function(percentAsDecimal){
   }
 };
 
-videojs.Youtube.prototype.muted = function() { return this.ytplayer.isMuted(); };
-videojs.Youtube.prototype.setMuted = function(muted) { 
+videojs.Quicktime.prototype.muted = function() {
+  console.log("Mute", this.qtplayer.GetMute());
+  return this.qtplayer.GetMute();
+};
+videojs.Quicktime.prototype.setMuted = function(muted) { 
   if (muted) {
     this.ytplayer.mute(); 
   } else { 
@@ -194,21 +242,26 @@ videojs.Youtube.prototype.setMuted = function(muted) {
   setTimeout(function() { self.player_.trigger('volumechange'); }, 50);
 };
 
-videojs.Youtube.prototype.onReady = function(){
+videojs.Quicktime.prototype.onReady = function(){
+  console.log("readying");
   this.isReady_ = true;
   this.player_.trigger('techready');
+
+  console.log("readied");
 
   // Hide the poster when ready because YouTube has it's own
   this.triggerReady();
   this.player_.trigger('durationchange');
+
+  console.log("durationchange");
   
   // Play right away if we clicked before ready
-  if (this.playOnReady){
-    this.ytplayer.playVideo();
-  }
+  // if (this.playOnReady){
+  //   this.ytplayer.playVideo();
+  // }
 };
 
-videojs.Youtube.prototype.onStateChange = function(state){
+videojs.Quicktime.prototype.onStateChange = function(state){
   if (state != this.lastState){
     switch(state){
       case -1:
@@ -243,7 +296,7 @@ videojs.Youtube.prototype.onStateChange = function(state){
   }
 };
 
-videojs.Youtube.prototype.onPlaybackQualityChange = function(quality){
+videojs.Quicktime.prototype.onPlaybackQualityChange = function(quality){
   switch(quality){
     case 'medium':
       this.player_.videoWidth = 480;
@@ -284,41 +337,41 @@ videojs.Youtube.prototype.onPlaybackQualityChange = function(quality){
   this.player_.trigger('ratechange');
 };
 
-videojs.Youtube.prototype.onError = function(error){
+videojs.Quicktime.prototype.onError = function(error){
   this.player_.error = error;
   this.player_.trigger('error');
 };
 
-videojs.Youtube.isSupported = function(){
+videojs.Quicktime.isSupported = function(){
   return true;
 };
 
-videojs.Youtube.prototype.supportsFullScreen = function() {
+videojs.Quicktime.prototype.supportsFullScreen = function() {
   return false;
 };
 
-videojs.Youtube.canPlaySource = function(srcObj){
+videojs.Quicktime.canPlaySource = function(srcObj){
   return (srcObj.type == 'video/youtube');
 };
 
 // All videos created before YouTube API is loaded
-videojs.Youtube.loadingQueue = [];
+videojs.Quicktime.loadingQueue = [];
 
 // Create the YouTube player
-videojs.Youtube.prototype.loadYoutube = function(){
-  this.ytplayer = new YT.Player(this.id_, {
-    events: {
-      onReady: function(e) { e.target.vjsTech.onReady(); },
-      onStateChange: function(e) { e.target.vjsTech.onStateChange(e.data); },
-      onPlaybackQualityChange: function(e){ e.target.vjsTech.onPlaybackQualityChange(e.data); },
-      onError: function(e){ e.target.vjsTech.onError(e.data); }
-    }
-  });
+videojs.Quicktime.prototype.loadQuicktime = function(){
+  // this.ytplayer = new YT.Player(this.id_, {
+  //   events: {
+  //     onReady: function(e) { e.target.vjsTech.onReady(); },
+  //     onStateChange: function(e) { e.target.vjsTech.onStateChange(e.data); },
+  //     onPlaybackQualityChange: function(e){ e.target.vjsTech.onPlaybackQualityChange(e.data); },
+  //     onError: function(e){ e.target.vjsTech.onError(e.data); }
+  //   }
+  // });
 
-  this.ytplayer.vjsTech = this;
+  // this.ytplayer.vjsTech = this;
 };
 
-videojs.Youtube.makeQueryString = function(args){
+videojs.Quicktime.makeQueryString = function(args){
   var array = [];
   for (var key in args){
     if (args.hasOwnProperty(key)){
@@ -328,15 +381,4 @@ videojs.Youtube.makeQueryString = function(args){
 
   return array.join('&');
 };
-
-// Called when YouTube API is ready to be used
-window.onYouTubeIframeAPIReady = function(){
-
-  var yt;
-  while ((yt = videojs.Youtube.loadingQueue.shift())){
-    yt.loadYoutube();
-  }
-  videojs.Youtube.loadingQueue = [];
-  videojs.Youtube.apiReady = true;
-}
 
